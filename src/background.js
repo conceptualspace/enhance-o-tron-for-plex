a6eac6c79bbc6dd4dacad02cdc08c56b407b918d
conceptualspace <dev@conceptualspace.net>
2021-05-07 02:30:39 -0400

    update message

--- src/background.js
+++ src/background.js
@@ -6,7 +6,7 @@ function handleInstalled(details) {
     if (details.reason === "install") {
         // set uninstall URL
         chrome.runtime.setUninstallURL("https://forms.gle/JqMEogANnkktEtSR9");
-    } else if (details.reason === "update" && details.previousVersion === "1.3.1") {
+    } else if (details.reason === "update" && details.previousVersion === "1.4.0") {
         const url = chrome.runtime.getURL("updated.html");
         chrome.tabs.create({ url });
     }
